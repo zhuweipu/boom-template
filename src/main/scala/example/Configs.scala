4e830de24e36aee4df4e78fdfce7dbd75b79c9df
Howard Mao <zhehao.mao@gmail.com>
2016-10-27 17:49:07 -0700

    add dma

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -2,6 +2,14 @@ package example
 
 import cde.{Parameters, Config, CDEMatchError}
 import testchipip.WithSerialAdapter
+import dma.WithDma
+import coreplex.WithL2Cache
 
 class DefaultExampleConfig extends Config(
   new WithSerialAdapter ++ new rocketchip.BaseConfig)
+
+class BaselineConfig extends Config(
+  new WithL2Cache ++ new DefaultExampleConfig)
+
+class DmaConfig extends Config(
+  new WithDma ++ new BaselineConfig)
