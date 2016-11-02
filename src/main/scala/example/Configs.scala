4bf128e89b4b0d2f35c3d322ccfab78f27dd2a4c
Howard Mao <zhehao.mao@gmail.com>
2016-11-02 16:19:27 -0700

    add design points for benchmarking

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -2,14 +2,37 @@ package example
 
 import cde.{Parameters, Config, CDEMatchError}
 import testchipip.WithSerialAdapter
-import dma.WithDma
-import coreplex.WithL2Cache
+import dma.{WithDma, NDmaTrackers}
+import coreplex.{WithL2Cache, WithPLRU}
+import rocketchip.{WithNMemoryChannels, BaseConfig}
+import uncore.agents.NAcquireTransactors
 
 class DefaultExampleConfig extends Config(
-  new WithSerialAdapter ++ new rocketchip.BaseConfig)
+  new WithSerialAdapter ++ new BaseConfig)
+
+class WithOneChannel extends WithNMemoryChannels(1)
+class WithTwoChannels extends WithNMemoryChannels(2)
+class WithFourChannels extends WithNMemoryChannels(4)
+
+class WithOneTracker extends WithNDmaTrackers(1)
+class WithTwoTrackers extends WithNDmaTrackers(2)
+class WithFourTrackers extends WithNDmaTrackers(4)
+
+class WithNAcquireTransactors(n: Int) extends Config(
+  (pname, site, here) => pname match {
+    case NAcquireTransactors => n
+    case _ => throw new CDEMatchError
+  })
 
 class BaselineConfig extends Config(
-  new WithL2Cache ++ new DefaultExampleConfig)
+  new WithNAcquireTransactors(8) ++
+  new WithPLRU ++ new WithL2Cache ++ new DefaultExampleConfig)
+
+class WithNDmaTrackers(n: Int) extends Config(
+  (pname, site, here) => pname match {
+    case NDmaTrackers => n
+    case _ => throw new CDEMatchError
+  })
 
 class DmaConfig extends Config(
   new WithDma ++ new BaselineConfig)
