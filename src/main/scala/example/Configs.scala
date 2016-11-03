03a5d894b45e15cc83fc7a97936fdeb26ce3f0a7
Howard Mao <zhehao.mao@gmail.com>
2016-11-03 10:07:53 -0700

    add groundtest configs

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -2,6 +2,10 @@ package example
 
 import cde.{Parameters, Config, CDEMatchError}
 import testchipip.WithSerialAdapter
+import groundtest.{WithGroundTest, WithMemtest}
 
 class DefaultExampleConfig extends Config(
   new WithSerialAdapter ++ new rocketchip.BaseConfig)
+
+class MemtestConfig extends Config(
+  new WithMemtest ++ new WithGroundTest ++ new DefaultExampleConfig)
