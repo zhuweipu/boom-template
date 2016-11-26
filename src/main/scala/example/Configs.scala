76425485747e4645c67c6e99e13567733918db13
Howard Mao <zhehao.mao@gmail.com>
2016-11-25 16:06:29 -0800

    shrink L2 cache to 256 KB

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -2,16 +2,26 @@ package example
 
 import cde.{Parameters, Config, CDEMatchError}
 import testchipip.WithSerialAdapter
-import dma.{WithDma, NDmaTrackers}
-import coreplex.{WithL2Cache, WithPLRU}
+import dma._
+import coreplex.{WithL2Cache, WithPLRU, WithL2Capacity}
 import rocketchip.{WithNMemoryChannels, BaseConfig}
 import uncore.agents.NAcquireTransactors
 import groundtest.{WithGroundTest, WithMemtest}
-import hwacha.DefaultHwachaConfig
+import hwacha._
 
 class WithOneChannel extends WithNMemoryChannels(1)
 class WithTwoChannels extends WithNMemoryChannels(2)
 class WithFourChannels extends WithNMemoryChannels(4)
+
+class WithNLanes(n: Int) extends Config(
+  (pname, site, here) => pname match {
+    case HwachaNLanes => n
+    case _ => throw new CDEMatchError
+  })
+
+class WithOneLane extends WithNLanes(1)
+class WithTwoLanes extends WithNLanes(2)
+class WithFourLanes extends WithNLanes(4)
 
 class WithOneTracker extends WithNDmaTrackers(1)
 class WithTwoTrackers extends WithNDmaTrackers(2)
@@ -23,9 +33,13 @@ class WithNAcquireTransactors(n: Int) extends Config(
     case _ => throw new CDEMatchError
   })
 
+class WithTwoAcquireTransactors extends WithNAcquireTransactors(2)
+class WithFourAcquireTransactors extends WithNAcquireTransactors(4)
+class WithEightAcquireTransactors extends WithNAcquireTransactors(8)
+
 class BaselineConfig extends Config(
-  new WithNAcquireTransactors(8) ++
-  new WithPLRU ++ new WithL2Cache ++ new DefaultExampleConfig)
+  new WithPLRU ++ new WithL2Capacity(256) ++
+  new WithL2Cache ++ new DefaultExampleConfig)
 
 class WithNDmaTrackers(n: Int) extends Config(
   (pname, site, here) => pname match {
@@ -33,8 +47,23 @@ class WithNDmaTrackers(n: Int) extends Config(
     case _ => throw new CDEMatchError
   })
 
+class WithPipelineDepth(depth: Int) extends Config(
+  (pname, site, here) => pname match {
+    case DmaTrackerPipelineDepth => depth
+    case _ => throw new CDEMatchError
+  })
+
+class WithPipelineDepth16 extends WithPipelineDepth(16)
+class WithPipelineDepth64 extends WithPipelineDepth(64)
+class WithPipelineDepth256 extends WithPipelineDepth(256)
+
 class DmaConfig extends Config(
   new WithDma ++ new BaselineConfig)
+
+class DmaTestConfig extends Config(
+  new WithDma ++ new WithL2Cache ++
+  new WithDmaTest ++ new WithGroundTest ++
+  new DefaultExampleConfig)
 
 class DefaultExampleConfig extends Config(
   new WithSerialAdapter ++ new rocketchip.BaseConfig)
