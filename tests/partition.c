bbfb781d34eb45fbe1f835f17e0377a2463e24e9
Howard Mao <zhehao.mao@gmail.com>
2017-03-01 15:13:42 -0800

    improve partitioner test

--- tests/partition.c
+++ tests/partition.c
@@ -2,7 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void checkEqual(uint32_t expected, uint32_t actual)
+// We are only feeding the partitioner 10 inputs, but there are two
+// extra spaces to keep inputColumn aligned
+int inputKeys[12] = {43, 5, 24, 77, 56, 265, 122, 104, 91, 145, 0, 0};
+// 16-byte column with 10 elements
+unsigned long inputColumn[20] = {
+	0xffaeL, 0xab35L, 0x8829L, 0xfe32L,
+	0x5521L, 0x56b3L, 0x671cL, 0x44ddL,
+	0x891aL, 0xbc32L, 0xab32L, 0x5321L,
+	0xbaccL, 0x3333L, 0x1023L, 0xbfa4L,
+	0xaaaaL, 0xddddL, 0x1026L, 0x3333L
+};
+// How does the input map to output
+int inputIdx[10] = {1, 2, 0, 4, 3, 7, 8, 6, 9, 5};
+
+int dividers[8] = {12, 32, 50, 60, 80, 110, 133, 160};
+
+int outputKeys[12];
+unsigned long outputColumn[20];
+int outputIdx[9] = {0, 1, 2, 3, 4, 5, 7, 8, 9};
+
+
+
+void checkEqualInt(int expected, int actual)
 {
 	if (expected == actual)
 		return;
@@ -10,18 +32,13 @@ void checkEqual(uint32_t expected, uint32_t actual)
 	exit(EXIT_FAILURE);
 }
 
-int inputKeys[10] = {43, 5, 24, 77, 56, 265, 122, 104, 91, 145};
-unsigned long inputColumn[10] = {
-	0xffaeL, 0xab35L, 0x8829L, 0xfe32L, 0x5521L,
-	0x56b3L, 0x671cL, 0x44ddL, 0x891aL, 0xbc32L
-};
-int inputIdx[10] = {1, 2, 0, 4, 3, 7, 8, 6, 9, 5};
-
-int dividers[8] = {12, 32, 50, 60, 80, 110, 133, 160};
-
-int outputKeys[10];
-unsigned long outputColumn[10];
-int outputIdx[9] = {0, 1, 2, 3, 4, 5, 7, 8, 9};
+void checkEqualLong(unsigned long expected, unsigned long actual)
+{
+	if (expected == actual)
+		return;
+	printf("Expected %x, got %x\n", expected, actual);
+	exit(EXIT_FAILURE);
+}
 
 int main(void)
 {
@@ -29,7 +46,7 @@ int main(void)
 
 	partition_set_columns(1);
 	partition_set_size(0, SIZE_32);
-	partition_set_size(1, SIZE_64);
+	partition_set_size(1, SIZE_128);
 	partition_set_input_addr(0, inputKeys);
 	partition_set_input_addr(1, inputColumn);
 
@@ -38,14 +55,15 @@ int main(void)
 	}
 	for (i = 0; i < 9; i++) {
 		partition_set_output_addr(i, 0, &outputKeys[outputIdx[i]]);
-		partition_set_output_addr(i, 1, &outputColumn[outputIdx[i]]);
+		partition_set_output_addr(i, 1, &outputColumn[2 * outputIdx[i]]);
 	}
 	partition_start(10);
 	asm volatile ("fence");
 
 	for (i = 0; i < 10; i++) {
-		checkEqual(inputKeys[inputIdx[i]], outputKeys[i]);
-		checkEqual(inputColumn[inputIdx[i]], outputColumn[i]);
+		checkEqualInt(inputKeys[inputIdx[i]], outputKeys[i]);
+		checkEqualLong(inputColumn[2 * inputIdx[i]], outputColumn[2 * i]);
+		checkEqualLong(inputColumn[2 * inputIdx[i] + 1], outputColumn[2 * i + 1]);
 	}
 
 	return 0;
