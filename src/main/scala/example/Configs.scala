16fc06d71c46d3aa5feaa94b9d3f9513ced53576
Howard Mao <zhehao.mao@gmail.com>
2017-03-19 11:21:29 -0700

    add 512-bit edge config for catapult testing

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -4,8 +4,8 @@ import cde.{Parameters, Config, CDEMatchError}
 import testchipip.WithSerialAdapter
 import dma._
 import coreplex.{WithL2Cache, WithPLRU, WithL2Capacity}
-import rocketchip.{WithNMemoryChannels, BaseConfig}
-import uncore.agents.NAcquireTransactors
+import rocketchip.{WithNMemoryChannels, BaseConfig, EdgeDataBits}
+import uncore.agents.{NAcquireTransactors, CacheBlockBytes}
 import groundtest.{WithGroundTest, WithMemtest}
 import adamacc.WithAdamAcc
 import hwacha._
@@ -76,3 +76,22 @@ class HwachaConfig extends Config(
 
 class AdamAccConfig extends Config(
   new WithAdamAcc ++ new BaselineConfig)
+
+class WithBigMemIF extends Config(
+  (pname, site, here) => pname match {
+    case CacheBlockBytes => 128
+    case EdgeDataBits => 512
+    case _ => throw new CDEMatchError
+  },
+  knobValues = {
+    case "L1D_SETS" => 32
+    case "L1I_SETS" => 32
+    case "L2_CAPACITY_IN_KB" => 16
+    case _ => throw new CDEMatchError
+  })
+
+class BigMemConfig extends Config(
+  new WithBigMemIF ++ new BaselineConfig)
+
+class AdamAccBigMemConfig extends Config(
+  new WithAdamAcc ++ new BigMemConfig)
