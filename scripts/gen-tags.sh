b0a6387f3e4aa18118773e89f06a582069dfdfa0
abejgonzalez <abe.j.gonza@gmail.com>
2018-11-30 10:06:42 -0800

    Updated script description and instructions

--- scripts/gen-tags.sh
+++ scripts/gen-tags.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 # run this script in the main boom-template directory to generate ctags for boom and other relevant repos
+# note: this requires exuberant-ctags
+# tested with: Exuberant Ctags 5.8
+# instructions:
+#    cd /path/to/boom-template/
+#    ./scripts/gen-tags.sh
 
 # ctags wrapper
 ctags -R --exclude=@.ctagsignore --links=no
