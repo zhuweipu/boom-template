285b9033dc81ac05d79e971ac1b54a99825dcab2
abejgonzalez <abe.j.gonza@gmail.com>
2019-02-04 10:49:15 -0800

    Update to project-template changes | Support multi-projects | Comments!

--- scripts/init-submodules-no-riscv-tools.sh
+++ scripts/init-submodules-no-riscv-tools.sh
@@ -2,10 +2,6 @@
 
 # Script for faster checkout if you already have installed your RISC-V tools.
 
-# top-level
-git submodule update --init
-# rocket-chip (skip tools)
-(cd rocket-chip && git submodule update --init --recursive hardfloat chisel3 firrtl)
-# Skip riscv-tools.
-# torture submodules
-(cd torture && git submodule update --init --recursive)
+git config --global submodule.riscv-tools.update none
+git submodule update --init --recursive
+git config --global --unset submodule.riscv-tools.update
