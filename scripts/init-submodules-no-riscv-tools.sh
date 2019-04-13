c1a61bf727474d2b3e194efedeee421ac0153f7f
abejgonzalez <abe.j.gonza@gmail.com>
2019-04-12 19:03:45 -0700

    add firrtl patch to fix const prop issues

--- scripts/init-submodules-no-riscv-tools.sh
+++ scripts/init-submodules-no-riscv-tools.sh
@@ -9,3 +9,6 @@ cd $BASE_DIR
 git config --global submodule.riscv-tools.update none
 git submodule update --init --recursive
 git config --global --unset submodule.riscv-tools.update
+
+# TODO: Remove FIRRTL patch in next rocket-bump (fixes const prop issue)
+git -C $BASE_DIR/rocket-chip/firrtl checkout 9535e03020c6e654dae3ce7e95f4d8649405ce3d
